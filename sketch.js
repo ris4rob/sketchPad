df1ee1e419d2862609169d098e3bca480f8aff84
ris4rob <108408741+ris4rob@users.noreply.github.com>
2023-07-18 20:08:43 +0800

    Refactored code

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
-const div = document.createElement('div');
 const sketchpad = document.querySelector('#sketchpad');
 const color = document.querySelector('#color');
 const eraser = document.querySelector('#eraser');
+const gridSelector = document.querySelector('#gridSelector');
 
 for (let i = 0; i < 256; i++) {
   const tempDiv = document.createElement('div');
@@ -19,7 +19,29 @@ grids.forEach((grid) => {
   });
 });
 
+gridSelector.addEventListener('change', (event) => {
+  while (sketchpad.firstChild) {
+    sketchpad.removeChild(sketchpad.firstChild);
+  }
+
+  const gridSize = event.target.value;
+  const gridCount =
+    gridSize === 'sixteen' ? 256 : gridSize === 'twentyfour' ? 576 : 1024;
+
+  for (let i = 0; i < gridCount; i++) {
+    const tempDiv = document.createElement('div');
+    tempDiv.classList.add('grid');
+    tempDiv.setAttribute('id', 'grid');
+    sketchpad.appendChild(tempDiv);
+
+    tempDiv.addEventListener('mouseenter', (event) => {
+      event.target.style.backgroundColor = color.value;
+    });
+  }
+});
+
 eraser.addEventListener('click', (event) => {
+  const grids = document.querySelectorAll('.grid');
   grids.forEach((grid) => {
     grid.style.backgroundColor = 'transparent';
   });
