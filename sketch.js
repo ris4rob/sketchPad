6d57b1ec36fbcb75bf81b21dd2f84d7f5f391161
ris4rob <108408741+ris4rob@users.noreply.github.com>
2023-08-01 12:30:42 +0800

    Refactor sketchpad from flex to grid and add new responsive input slide

--- sketch.js
+++ sketch.js
@@ -2,15 +2,6 @@ const sketchpad = document.querySelector('#sketchpad');
 const color = document.querySelector('#color');
 const eraser = document.querySelector('#eraser');
 const gridSelector = document.querySelector('#gridSelector');
-
-for (let i = 0; i < 256; i++) {
-  const tempDiv = document.createElement('div');
-  tempDiv.classList.add('grid');
-  tempDiv.setAttribute('id', 'grid');
-  //   tempDiv.innerText = i;
-  sketchpad.appendChild(tempDiv);
-}
-
 const grids = document.querySelectorAll('#grid');
 
 grids.forEach((grid) => {
@@ -24,11 +15,15 @@ gridSelector.addEventListener('change', (event) => {
     sketchpad.removeChild(sketchpad.firstChild);
   }
 
-  const gridSize = event.target.value;
-  const gridCount =
-    gridSize === 'sixteen' ? 256 : gridSize === 'twentyfour' ? 576 : 1024;
+  let rows = gridSelector.value;
+  let columns = gridSelector.value;
+  document.documentElement.style.setProperty('--columns', columns);
+  document.documentElement.style.setProperty('--rows', rows);
+  createDivs(rows, columns);
+});
 
-  for (let i = 0; i < gridCount; i++) {
+function createDivs(rows, columns) {
+  for (let i = 0; i < rows * columns; i++) {
     const tempDiv = document.createElement('div');
     tempDiv.classList.add('grid');
     tempDiv.setAttribute('id', 'grid');
@@ -38,7 +33,7 @@ gridSelector.addEventListener('change', (event) => {
       event.target.style.backgroundColor = color.value;
     });
   }
-});
+}
 
 eraser.addEventListener('click', (event) => {
   const grids = document.querySelectorAll('.grid');
@@ -46,3 +41,5 @@ eraser.addEventListener('click', (event) => {
     grid.style.backgroundColor = 'transparent';
   });
 });
+
+document.addEventListener('loaded', createDivs(5, 5));
